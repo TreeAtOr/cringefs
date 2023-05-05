1cef19da9df159b57209967da0c72f152d75fc19
jaroslav2324 <jaro_r@mail.ru>
2023-05-05 07:53:31 +0300

    first commit

---
+++ cringefs.c
@@ -0,0 +1,184 @@
+#include "cringefs.h"
+
+int cringefs_f_descriptor = -1;
+struct cringefs_super_block sb;
+struct cringefs_file_table ft;
+
+
+int main(int argc, char* argv[]){
+    // argv[1] это путь к файлу устройства(флешки) ( например "/dev/sda3" )
+
+    if (argc <= 1){
+        //print argument with device path missed
+        return -1;
+    }
+    // startup(argv[1])
+
+    //if(check_sb() == -1){
+    // print super block corrupted / no fs on device
+    // ask to continue
+    //}
+
+    
+    // parse_args()
+
+    // поймать сигнал ctrl+c? дла корректного shutdown
+    // shutdown()
+
+    return 0;
+}
+
+int startup(char* device_path, struct cringefs_super_block* sb){
+    // cringefs_f_descriptor = open(device_path)  // гуглите функцию open()
+    // read sb
+    check_sb(sb);
+    // if something bad return -1
+}
+
+int shutdown(){
+    // close(cringefs_f_descriptor) ??? 
+    // if something bad return -1
+}
+
+int check_sb(struct cringefs_super_block* sb){
+    // check magic
+    // if != CRINGEFS_MAGIC return -1
+}
+
+
+
+
+void parse_args(){
+
+    //while (1)
+    //{
+        // read str, parse_str(char* str, ...);
+        // if bad str, message
+        // if good, exec_command() (use switch and cringefs_command_types)
+        // if exit return
+    //}
+    
+}
+
+// parse str, put command in dst_command. If something goes wrong return -1
+int parse_str(char * str, struct cringefs_command* dst_command){
+    // check str if it is command and it has correct arguments
+    //fill dst_command struct
+    
+}
+
+int exec_command(struct cringefs_command* command){
+
+    switch (command->command_type)
+    {
+    case OPEN:
+        open_file(command->arg1);
+        break;
+    case CLOSE:
+        close_file(command->arg1);
+        break;
+    case SHOW:
+        show_file(command->arg1);
+        break;
+    case DELETE:
+        
+    case EXIT:
+        return 0;
+    default:
+        // print err
+        break;
+    }
+}
+
+int open_file(char* path){
+    // if file already in table return 0
+    // read file paths from disk until you find path
+    // if path not found return -1
+    // read file, add file to table
+    
+}
+
+int close_file(char* path){
+    // read table until you find file with path
+    // if file has been modified save it
+    // clear it
+}
+
+int show_file(char* path){
+
+    // find file with path in table
+    // show on screen
+    // if not found
+    // find file on disk
+    // show on screen
+    // if not foud
+    // print no such file
+}
+
+int write_file(char* path){
+    // find file with path in table
+    // if not found return -1
+    // else just write to disk
+}
+
+int extend_file(char* path){
+    // think 
+}
+
+int shrink_file(char* path){
+    // think
+}
+
+int copy_file(char* path, char* dst_path){
+    // find file by path in table/disk
+    // if not found print, return -1 
+    // check existance of dst_path
+    // if exists print, return -1
+    // create new meta, blocks
+    // copy data
+}
+
+int move_file(char* path, char* dst_path){
+
+    // copy_file()
+    // delete file with path
+}
+
+int delete_file(char* path){
+    //find file in table
+    // unload()
+    //find file on disk
+    // set meta cleared
+}
+
+
+int sort_meta(){
+    // hardcore
+}
+int pack_fs(){
+    // hardcore
+}
+
+int format_fs(){
+    // clear table
+    // reset superblock
+    // write sb
+}
+
+int remove_from_table(char* path){
+    // find and clear
+}
+
+int clear_table(){
+    //clear all
+}
+
+//return ptr if found file else -1
+struct cringefs_file* find_file_table(char* path){
+    // find in table
+}
+
+// return ptr if found file else -1
+int* find_file_disk(char* path){
+    // find on disk
+}
---
+++ cringefs.h
@@ -0,0 +1,94 @@
+#include "stdio.h"
+#include "string.h"
+
+#define CRINGEFS_STARTPOS 0xffff // for formating system
+
+#define CRINGEFS_MAGIC 0x69A4
+
+#define CRINGEFS_FILE_PATH_LEN 256
+
+struct cringefs_super_block{
+    int sb_magic;
+    /* data */
+};
+
+struct cringefs_meta{
+    char f_path[CRINGEFS_FILE_PATH_LEN];
+    // char is_dir = 0/1
+    // ptr to start block
+    // amount blocks
+    /* data */
+};
+
+struct cringefs_file_table{
+    // array/list of loaded files
+    // amount?
+    /* data */
+};
+
+struct cringefs_file{
+    // ptr to meta
+    // ptr to blocks
+};
+
+// need?
+struct cringefs_block{
+    // ptr to block?
+};
+
+enum cringefs_command_types{
+    OPEN,     // open  /text.txt
+    CLOSE,    // close /text.txt
+    SHOW,     // cat   /text.txt
+    DELETE,   // rm    /text.txt
+    CREATE,   // touch /text.txt
+    COPY,     // cp    /text.txt /text2.txt
+    MOVE,     // mv    /text.txt /dir/text.txt
+    CREATEDIR,// mkdir /dir
+    FORMAT,   // format
+    EXTEND,   // need command or it is inner function?
+    SHRINK,   // need_command or it is inner function?
+    EXIT      // exit
+    // etc.
+};
+
+struct cringefs_command{
+    enum cringefs_command_types command_type;
+    char num_args;
+    char* arg1;
+    char* arg2;
+    //char* arg3;
+    //char* arg4;
+};
+
+int startup(char* device_path, struct cringefs_super_block* sb);
+int shutdown();
+int check_sb(struct cringefs_super_block* sb);
+
+void parse_args();
+int parse_str(char * str, struct cringefs_command* dst_command);
+int exec_command(struct cringefs_command* command);
+
+int open_file(char* path); // aka load to table
+int close_file(char* path); // aka unload from table
+int show_file(char* path); // show on screen
+int write_file(char* path); // save to disk
+int extend_file(char* path); // move? file and add free blocks
+int shrink_file(char* path); // remove blocks
+int copy_file(char* path, char* dst_path); // copy file from path to dst_path
+int move_file(char* path, char* dst_path); // move file from path to dst path
+int delete_file(char* path); // delete from disk
+struct cringefs_file* find_file_table(char* path); // returns ptr to file
+int* find_file_disk(char* path); // returns ptr to file on disk
+
+int sort_meta();
+int pack_fs();
+
+int format_fs();
+
+int add_to_table(struct cringefs_file* file);
+int remove_from_table(char* path);
+int clear_table();
+
+//read_block ?
+// write block ? 
